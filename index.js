0c2b2f56cfee47dde993991a36f804c6c6270ab3
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-11 23:31:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function findMatching(drivers, string) {
     return driver === string;
   })
 }
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(element => element.startsWith(string))
+}
+
+function matchName(arrOfObjects, string) {
+  return arrOfObjects.filter(obj => obj.name === string)
+}
