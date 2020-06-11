1ce266c3bcb0aeb2e9cfcee8063c0a587e41701b
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-11 23:30:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here
 function findMatching(drivers, string) {
-  let availableDrivers = arr.filter(n => {
-    return n % 2 === 0;
+  let availableDrivers = drivers.filter(driver => {
+    return driver === string;
   })
 }
